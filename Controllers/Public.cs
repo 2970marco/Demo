56a360d45ae563e11f7366088eb1ad598044f531
2970marco <2970marco@gmail.com>
2024-12-22 19:43:05 +0200

    Add project files.

---
+++ Controllers/Public.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Public : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet("Version")]
+        public object Version()
+        {
+            return new { Version = "1.30" };
+        }
+
+        [AllowAnonymous]
+        [HttpGet("AppSettings")]
+        public object GetAppSettings()
+        {
+            return AppSettings.Settings;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("TestException")]
+        public void TestException()
+        {
+            throw new Exception("Exception occurred!");
+        }
+
+        [Authorize]
+        [HttpGet("TestAuthentication")]
+        public ActionResult TestAuthentication()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet("{numOfWf}")]
+        public IEnumerable<WeatherForecast> Get(int numOfWf)
+        {
+            var rng = new Random();
+
+            //if (numOfWf % 2 == 0)
+            //{
+            //    numOfWf += 1;
+            //}
+
+            //if (numOfWf > 10)
+            //{
+            //    System.Threading.Thread.Sleep(5000);
+            //}
+
+            return Enumerable.Range(1, numOfWf).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+    }
+}
